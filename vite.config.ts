6f0cb2483bf5602d670d7919d7e8fd6dc782c4b4
toma <23054830+toma-to@users.noreply.github.com>
2024-06-29 21:30:08 +0900

    アイコンフォントを拡張機能内に持つよう修正

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   define: {
     __VUE_PROD_DEVTOOLS__: true,
   },
-  plugins: [vue()],
+  plugins: [vue(),
+    
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'assets/*',
+          dest: 'assets'
+        }
+      ]
+    })
+  ],
 });
